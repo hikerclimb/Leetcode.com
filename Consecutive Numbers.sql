586cc96cdab6bbb46e82c2130371c124488503a7
Aniket Karmarkar <karmarkaraniket@gmail.com>
2026-05-27 10:04:06 -0500

    got 10 out of 23 test cases working :)

--- Consecutive Numbers.sql
+++ Consecutive Numbers.sql
@@ -1,35 +1,3 @@
-delimeter $$ 
-drop procedure if exists 'consecutive' $$
-create procedure consecutive(out output int)
-begin
-    declare val INT default 0;
-    declare prevVal INT default 0;
-    declare temp INT default 0;
-    declare count int default 0;
-    declare iterate_over CURSOR for select num from Logs;
-
-
-    OPEN iterate_over;
-
-    read_loop: LOOP
-        fetch next from iterate_over into val;
-        if prevVal = next then
-            set temp = next;
-            set val = temp;
-            set count = count + 1;
-        else
-            set prevVal = next;
-            set count = 0;
-        end if;
-        
-        if count = 2 then
-            set output = next;
-            LEAVE read_loop;
-        end if;
-    end loop;
-    CLOSE iterate_over;
-end $$
-
-CALL consecutive(@output)
-
-SELECT @output as ConsecutiveNums;
+select
+if((num = lead(num, 1,NULL) OVER()) and (num = lag(num,1, NULL) OVER()),num, NULL)  
+as ConsecutiveNums from Logs limit 1,1
